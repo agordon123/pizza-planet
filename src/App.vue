5000bd8e6a5a310725094e6995805f860bc061b5
Adam Gordon <adamg42685@gmail.com>
2022-12-30 08:35:50 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<appHeader>
+<appHome>
+
+</appHome>
+</appHeader>
+
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header';
+import HomePage from './components/HomePage';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    appHeader: Header,
+    appHome: HomePage
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="background">
+    <div class="background_overlay">
+        <h2> Welcome to Pizza Planet!</h2>
+        <img src="../assets/images/roller.png" alt="roller" />
+        <h3>Feeling Hungry?</h3>
+        <button class="order_btn">Let's Order</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name:"HomePage"
+};
+</script>
+
+<style scoped>
+
+</style>
